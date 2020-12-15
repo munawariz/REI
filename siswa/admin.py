46dd52626c0f864af50cbeb34243e997e649ea27
harizMunawar <munawarhariz@gmail.com>
2020-12-15 18:04:36 +0700

    Create Nilai model.
    It is a transaction table for Nilai and MataPelajaran

--- siswa/admin.py
+++ siswa/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Siswa
+from .models import Siswa, Nilai
 
 @admin.register(Siswa)
 class SiswaAdmin(admin.ModelAdmin):
     list_display = ('nama', 'nisn', 'nis', 'gender', 'kelas')
     search_fields = ('nama', 'nisn', 'nis', 'gender', 'kelas')
     ordering = ('kelas', 'nama', 'gender')
+
+@admin.register(Nilai)
+class NilaiAdmin(admin.ModelAdmin):
+    list_display = ('semester', 'siswa', 'matapelajaran', 'pengetahuan', 'keterampilan')
+    search_fields = ('semester', 'siswa', 'matapelajaran', 'pengetahuan', 'keterampilan')
+    ordering = ('semester', 'siswa', 'matapelajaran')
--- siswa/models.py
+++ siswa/models.py
@@ -1,7 +1,8 @@
-from helpers.choice import GENDER_CHOICE
 from django.db import models
-from django.db.models.fields import CharField
-from sekolah.models import Kelas, Tingkat
+from sekolah.models import Kelas, Tingkat, MataPelajaran, Semester
+from helpers.choice import GENDER_CHOICE
+from django.core.exceptions import ValidationError
+from django.dispatch import receiver
 
 class Siswa(models.Model):
     nama = models.CharField(max_length=255, verbose_name='Nama Siswa')
@@ -14,13 +15,38 @@ class Siswa(models.Model):
     anak_ke = models.SmallIntegerField()
     alamat = models.TextField()
     sekolah_asal = models.CharField(max_length=255)
-    diterima_di_tingkat = models.ForeignKey(Tingkat, on_delete=models.SET_NULL, null=True)
+    diterima_di_tingkat = models.ForeignKey(Tingkat, on_delete=models.SET_NULL, null=True, related_name='siswa')
     nama_ayah = models.CharField(max_length=255)
     nama_ibu = models.CharField(max_length=255)
     nama_wali = models.CharField(max_length=255, null=True, blank=True)
-    kelas = models.ForeignKey(Kelas, on_delete=models.PROTECT)
+    kelas = models.ForeignKey(Kelas, on_delete=models.PROTECT, related_name='siswa')
 
     def __str__(self):
         return f'{self.nisn}/{self.nis}-{self.nama}'
+
+class Nilai(models.Model):
+    siswa = models.ForeignKey(Siswa, on_delete=models.CASCADE, related_name='nilai')
+    matapelajaran = models.ForeignKey(MataPelajaran, on_delete=models.CASCADE, related_name='nilai')
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, related_name='nilai', editable=False)
+    pengetahuan = models.SmallIntegerField()
+    keterampilan = models.SmallIntegerField()
+
+    def __str__(self):
+        return f'{self.siswa}/{self.matapelajaran}/{self.semester}'
+
+    def save(self, *args, **kwargs):
+        if self.pengetahuan < 0: self.pengetahuan = 0
+        if self.pengetahuan > 100: self.pengetahuan = 100
+        if self.keterampilan < 0: self.keterampilan = 0
+        if self.keterampilan > 100: self.keterampilan = 100
+
+        self.semester = self.siswa.kelas.semester
+        super(Nilai, self).save(*args, **kwargs)
+
+@receiver(models.signals.pre_save, sender=Nilai)
+def unique_together_tp_mapel(sender, instance, **kwargs):
+    nilai = Nilai.objects.filter(siswa=instance.siswa, matapelajaran=instance.matapelajaran, semester=instance.semester)
+    if nilai and instance not in nilai:
+        raise ValidationError('Nilai for that Siswa with that Mata Pelajaran in that Semester already exists')
 
     
