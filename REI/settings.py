9328f600de8913d6bd8ad19c38172828fc392d79
harizMunawar <munawarhariz@gmail.com>
2021-01-15 01:20:32 +0700

    Improve manage.py command with database selection argument.

--- REI/settings.py
+++ REI/settings.py
@@ -15,7 +15,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,16 +78,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': str(BASE_DIR / 'db.sqlite3'),
     },
-    'mongo': {
-        'ENGINE': 'djongo',
-        'NAME': 'REIMongo',
-        "CLIENT": {
-            "host": 'mongodb://admin-munawar:L1GvzLE7C8VuCsUA@rei-database-shard-00-00.94yhc.mongodb.net:27017,rei-database-shard-00-01.94yhc.mongodb.net:27017,rei-database-shard-00-02.94yhc.mongodb.net:27017/REIMongo?ssl=true&replicaSet=atlas-770ftc-shard-0&authSource=admin&retryWrites=true&w=majority',
-            "username": 'admin-munawar',
-            "password": 'L1GvzLE7C8VuCsUA',
-            "authMechanism": "SCRAM-SHA-1",
-        },
-    }
 }
 
 # Password validation
--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REI.settings')
+    if os.environ.get('MONGO'):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REI.mongo')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REI.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
