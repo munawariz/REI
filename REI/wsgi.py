183492896bace485382b0ee34b1118b3a9adc7e1
harizMunawar <munawarhariz@gmail.com>
2021-02-12 19:24:43 +0700

    Change wsgi

--- REI/wsgi.py
+++ REI/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REI.postgre')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REI.herokupostgre')
 
 application = get_wsgi_application()
